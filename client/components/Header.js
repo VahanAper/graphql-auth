69b2da3e63624fa9ad797fee1b4dfcf09c2b7a4b
VahanAper <asryanvahandev@gmail.com>
2017-06-27 17:29:49 +0400

    query for CurretnUser in Header Component

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+
+import query from '../queries/CurrentUser';
 
 class Header extends Component {
   render() {
+    console.log('this.props.data', this.props.data);
+
     return (
-      <div>Header</div>
+      <div>HEADER</div>
     );
   }
 }
 
-export default Header;
+export default graphql(query)(Header);
