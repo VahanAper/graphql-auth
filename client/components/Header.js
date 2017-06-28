c38835a71cd249f0f7eced2a3af01bed2191e0e6
VahanAper <asryanvahandev@gmail.com>
2017-06-28 10:43:47 +0400

    add navigation links into Header Component considering user auth state

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,14 +1,47 @@
 import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
+import  { Link } from 'react-router';
 
 import query from '../queries/CurrentUser';
 
 class Header extends Component {
-  render() {
-    console.log('this.props.data', this.props.data);
+  renderButtons() {
+    const { loading, user } = this.props.data;
+
+    if (loading) {
+      return <div />;
+    }
+    if (user) {
+      return <div>Logout</div>;
+    }
 
     return (
-      <div>HEADER</div>
+      <div>
+        <li>
+          <Link to="/signup">SignUp</Link>
+        </li>
+        <li>
+          <Link to="/login">LogIn</Link>
+        </li>
+      </div>
+    );
+  }
+
+  render() {
+    return (
+      <nav>
+        <div className="nav-wrapper">
+          <Link
+            to="/"
+            className="brand-logo left"
+          >
+            Home
+          </Link>
+          <ul className="right">
+            {this.renderButtons()}
+          </ul>
+        </div>
+      </nav>
     );
   }
 }
