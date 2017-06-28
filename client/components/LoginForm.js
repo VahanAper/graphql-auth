1c539b9292dc69f205c2a0c79718cbb3e9c9ad5f
VahanAper <asryanvahandev@gmail.com>
2017-06-28 11:29:16 +0400

    init AuthForm Component

--- client/components/LoginForm.js
+++ client/components/LoginForm.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 
+import AuthForm from './AuthForm';
+
 class LoginForm extends Component {
   render() {
     return (
-      <div>LoginForm</div>
+      <div>
+        <h3>LogIn</h3>
+        <AuthForm />
+      </div>
     );
   }
 }
