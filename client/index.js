1e25088d0825943c66fa1fe79d4861a8938d2fdd
VahanAper <asryanvahandev@gmail.com>
2017-06-28 11:14:19 +0400

    inint LoginForm Component

--- client/index.js
+++ client/index.js
@@ -10,6 +10,7 @@ import {
 } from 'react-router';
 
 import App from './components/App';
+import LoginForm from './components/LoginForm';
 
 const networkInterface = createNetworkInterface({
   uri: '/graphql',
@@ -27,7 +28,9 @@ const Root = () => {
   return (
     <ApolloProvider client={client}>
       <Router history={hashHistory}>
-        <Route path="/" component={App} />
+        <Route path="/" component={App}>
+          <Route path="/login" component={LoginForm} />
+        </Route>
       </Router>
     </ApolloProvider>
   );
