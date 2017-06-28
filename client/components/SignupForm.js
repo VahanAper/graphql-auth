e6e74b3e4b8c064802fa41c18ee6e770ed6b876e
VahanAper <asryanvahandev@gmail.com>
2017-06-28 14:24:12 +0400

    handle SignupForm submit

--- client/components/SignupForm.js
+++ client/components/SignupForm.js
@@ -5,11 +5,26 @@ import AuthForm from './AuthForm';
 import mutation from '../mutations/Signup';
 
 class SignupForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onSubmit({ email, password }) {
+    this.props.mutate({
+      variables: { email, password },
+    });
+  }
+
   render() {
     return (
       <div>
         <h3>SignUp</h3>
-        <AuthForm />
+        <AuthForm
+          errors={[]}
+          onSubmit={this.onSubmit}
+        />
       </div>
     );
   }
